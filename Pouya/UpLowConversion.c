c7882b640b1c6ff32783615ab2540cab617cae86
mahdigho@grinnell.edu <mahdigho@grinnell.edu>
2017-01-26 14:53:06 -0600

    Setting up the buffer

---
+++ Pouya/UpLowConversion.c
@@ -0,0 +1,10 @@
+/* Program to convert all uppercase letters to lowercase ones in a standard
+*     text file.
+*/
+#include <stdio.h>
+#include <string.h>
+
+#define buffersize 1000
+
+
+char buffer[buffersize];
