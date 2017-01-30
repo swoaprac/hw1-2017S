3f622f254d2edc73c209585e2ac2801749e2ae86
Sarah Trop <sctrop@gmail.com>
2017-01-30 17:43:23 -0600

    Fixing formatting to conform to GNU standards

--- Sarah/grades.c
+++ Sarah/grades.c
@@ -4,7 +4,7 @@
 
 // Find the current lowest grade in the top 5 grades
 int 
-get_lowest_grade(int highest_grades[])
+get_lowest_grade (int highest_grades[])
 {
 	int lowest = 110; // grades cannot be higher than 100
 	for (int i = 0; i < 5; i++) {
@@ -17,9 +17,9 @@ get_lowest_grade(int highest_grades[])
 
 // Updates the 5 highest grades with the new grade
 void
-update_highest_grades(int grade, int highest_grades[]) 
+update_highest_grades (int grade, int highest_grades[]) 
 {
-	int lowest = get_lowest_grade(highest_grades);
+	int lowest = get_lowest_grade (highest_grades);
 	int flag = 1;
 	for (int i = 0; i < 5; i++) {
 		if(grade < highest_grades[i]) {
@@ -29,9 +29,9 @@ update_highest_grades(int grade, int highest_grades[])
 }
 
 // Splits input string into parts
-void split_student_string(char* student, char student_split[])
+void split_student_string (char *student, char student_split[])
 {
-	char* token;
+	char *token;
 	int i = 0;
 	token = strtok(student,",");
 	while(token != NULL) {
@@ -44,26 +44,27 @@ void split_student_string(char* student, char student_split[])
 int
 main()
 {
-	char* student = (char*)malloc(1024);
-	char* assignment = (char*)malloc(1024);
+	char *student = (char*)malloc (1024);
+	char *assignment = (char*)malloc (1024);
 	int highest_grades[5] = {0,0,0,0,0};
-	char* student_split = (char*)malloc(sizeof(char*)*4);
+	char *student_split = (char*)malloc (sizeof(char*)*4);
 
-	FILE* input = fopen("input", "r");
-	while(fgets(student, 1024, input))
+	FILE* input = fopen ("input", "r");
+	while(fgets (student, 1024, input))
 	{
-		split_student_string(student, student_split);
-		if (strcmp(&student_split[2], "homework 2") == 0) {
-			update_highest_grades(student_split[3], highest_grades);
+		split_student_string (student, student_split);
+		if (strcmp (&student_split[2], "homework 2") == 0) {
+			update_highest_grades (student_split[3], highest_grades);
 		}
 	}
 
-	printf("The five highest grades on homework 2 are: %d, %d, %d, %d, %d\n", 
-		highest_grades[0], highest_grades[1], highest_grades[2], highest_grades[3], highest_grades[4]);
+	printf ("The five highest grades on homework 2 are: %d, %d, %d, %d, %d\n", 
+			highest_grades[0], highest_grades[1], highest_grades[2], 
+			highest_grades[3], highest_grades[4]);
 
-	free(student);
-	free(assignment);
-	free(student_split);
-	fclose(input);
+	free (student);
+	free (assignment);
+	free (student_split);
+	fclose (input);
 	return 0;
 }
--- Sarah/urls.c
+++ Sarah/urls.c
@@ -4,38 +4,38 @@
 
 // Finds and returns URL from a line
 void
-find_url(char* line, char* url) 
+find_url (char* line, char* url) 
 {
-	char* img_text = "<img";
-	char* img_location = strstr(line, img_text);
+	char *img_text = "<img";
+	char *img_location = strstr (line, img_text);
 
-	char* source_text = "*src=\"*\"*>";
-	char* source_location = strstr(img_location, source_text);
+	char *source_text = "*src=\"*\"*>";
+	char *source_location = strstr (img_location, source_text);
 
 	*source_location += 1;
-	size_t len = strlen(source_location) - 1;
-	url = strncpy(source_location, url, len);
+	size_t len = strlen (source_location) - 1;
+	url = strncpy (source_location, url, len);
 }
 
 int
-main(int argc, char const *argv[]) 
+main () 
 {
-	char* line = (char*)malloc(1024);
-	char* url = malloc(1024);
-  	FILE* input = fopen("input", "r");
+	char *line = (char*)malloc (1024);
+	char *url = malloc (1024);
+  	FILE *input = fopen ("input", "r");
   	// checking that the file exists
   	if (input == NULL) {
-  		printf("No such file\n");
-  		exit(1);
+  		printf ("No such file\n");
+  		exit (1);
   	}
-	while(fgets(line, 1024, input))
+	while(fgets (line, 1024, input))
 	{
-		find_url(line, url);
-		printf("%s\n", url);
+		find_url (line, url);
+		printf ("%s\n", url);
 	}
 
-  	fclose(input);
-  	free(line);
-  	free(url);
+  	fclose (input);
+  	free (line);
+  	free (url);
   	return 0;
 }
